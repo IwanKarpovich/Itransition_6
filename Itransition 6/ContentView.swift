0b20cf203ccf65b19c72c453bd493098dc302e6c
Ivan <ivan44364yr@gmail.com>
2021-12-24 23:02:21 +0300

    Initial Commit

---
+++ Itransition 6/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Itransition 6
+//
+//  Created by Ivan Karpovich on 24.12.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Itransition 6/Itransition_6App.swift
@@ -0,0 +1,17 @@
+//
+//  Itransition_6App.swift
+//  Itransition 6
+//
+//  Created by Ivan Karpovich on 24.12.21.
+//
+
+import SwiftUI
+
+@main
+struct Itransition_6App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
